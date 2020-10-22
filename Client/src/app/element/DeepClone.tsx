5afa1b13b2ba05896fef21f11fcdce42931ccda0
Yuriy Sountsov <yuriys@kentico.com>
2020-10-21 23:30:17 -0400

    Simplify element height

--- Client/src/app/element/DeepClone.tsx
+++ Client/src/app/element/DeepClone.tsx
@@ -31,14 +31,13 @@ export const DeepClone: FC = () => {
   const [enabled, setEnabled] = useState(true);
   const [customElementConfig, setCustomElementConfig] = useState<IDeepCloneConfig | null>(null);
   const [customElementContext, setCustomElementContext] = useState<IContext | null>(null);
+
   const [loading, setLoading] = useState(false);
   const [loaded, setLoaded] = useState(false);
   const [totalApiCalls, setTotalApiCalls] = useState(0);
   const [totalTime, setTotalTime] = useState<Duration>();
   const [newItems, setNewItems] = useState<IContentItem[]>([]);
   const [error, setError] = useState<string>();
-
-  const elementRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (!available) {
@@ -60,10 +59,8 @@ export const DeepClone: FC = () => {
   }, [available]);
 
   useEffect(() => {
-    if (available && elementRef.current) {
-      let totalHeight = elementRef.current.scrollHeight;
-
-      CustomElement.setHeight(totalHeight);
+    if (available) {
+      CustomElement.setHeight(document.documentElement.scrollHeight);
     }
   });
 
@@ -116,60 +113,56 @@ export const DeepClone: FC = () => {
   return (
     <div>
       {loading && <Loading />}
-      {available && (
-        <div ref={elementRef}>
-          {error && <div>{error}</div>}
-          {error === undefined && enabled && (
+      {error && <div>{error}</div>}
+      {error === undefined && available && enabled && (
+        <>
+          <div className={styles.row}>
+            <div className={styles.fullWidthCell}>
+              <p>{element.enabledDescription}</p>
+            </div>
+            <div>
+              <button className='btn btn--primary btn--xs' onClick={cloneElement}>
+                {element.button}
+              </button>
+            </div>
+          </div>
+          {loaded && customElementContext && totalTime && (
             <>
               <div className={styles.row}>
-                <div className={styles.fullWidthCell}>
-                  <p>{element.enabledDescription}</p>
-                </div>
-                <div>
-                  <button className='btn btn--primary btn--xs' onClick={cloneElement}>
-                    {element.button}
-                  </button>
+                <div className='content-item-element__name-status'>
+                  <label className='content-item-element__label'>{element.totalTime}</label>
+                  <p>{getTotalTimeString}</p>
                 </div>
               </div>
-              {loaded && customElementContext && totalTime && (
-                <>
-                  <div className={styles.row}>
-                    <div className='content-item-element__name-status'>
-                      <label className='content-item-element__label'>{element.totalTime}</label>
-                      <p>{getTotalTimeString}</p>
-                    </div>
-                  </div>
-                  <div className={styles.row}>
-                    <div className='content-item-element__name-status'>
-                      <label className='content-item-element__label'>{element.totalApiCalls}</label>
-                      <p>{totalApiCalls}</p>
-                    </div>
-                  </div>
-                  <div className={styles.row}>
-                    <div className='content-item-element__name-status'>
-                      <label className='content-item-element__label'>{element.newItems}</label>
-                      {newItems.map((item) => (
-                        <p key={item.id}>
-                          <a
-                            href={`https://app.kontent.ai/${customElementContext.projectId}/content-inventory/${customElementContext.variant.id}/content/${item.id}`}
-                            target='_blank'
-                            rel='noopener noreferrer'
-                          >
-                            {item.name}
-                          </a>
-                        </p>
-                      ))}
-                    </div>
-                  </div>
-                </>
-              )}
+              <div className={styles.row}>
+                <div className='content-item-element__name-status'>
+                  <label className='content-item-element__label'>{element.totalApiCalls}</label>
+                  <p>{totalApiCalls}</p>
+                </div>
+              </div>
+              <div className={styles.row}>
+                <div className='content-item-element__name-status'>
+                  <label className='content-item-element__label'>{element.newItems}</label>
+                  {newItems.map((item) => (
+                    <p key={item.id}>
+                      <a
+                        href={`https://app.kontent.ai/${customElementContext.projectId}/content-inventory/${customElementContext.variant.id}/content/${item.id}`}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                      >
+                        {item.name}
+                      </a>
+                    </p>
+                  ))}
+                </div>
+              </div>
             </>
           )}
-          {error === undefined && !enabled && (
-            <div className='content-item-element__guidelines'>
-              <p>{element.disabledDescription}</p>
-            </div>
-          )}
+        </>
+      )}
+      {error === undefined && !enabled && (
+        <div className='content-item-element__guidelines'>
+          <p>{element.disabledDescription}</p>
         </div>
       )}
     </div>
